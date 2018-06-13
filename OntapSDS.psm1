27b8d0a80207ce0ea1567412dba8367d64b878c7
Fabian Born <fabian.born@fabianborn.net>
2018-06-13 14:31:57 +0200

    Update 13.06.2018:14:31:57

--- OntapSDS.psm1
+++ OntapSDS.psm1
@@ -1,5 +1,10 @@
 set-Location "/Users/fabian/devel/Powershell/OntapSDS"
- 
+
+ipmo dataontap -ErrorAction SilentlyContinue
+if (!(Get-Module -Name Dataontap)){
+    Write-host -ForegroundColor yellow "The Data ONTAP Powershell toolkit couldn't be found. If you want to configure ONTAP by Powershell please install the Powershell Toolkit."
+}
+
 Get-ChildItem ".\lib" | Where {$_.Name -like "*.ps1"} | ForEach {
  
     Write-Host "[Including $_]" -ForegroundColor Green
@@ -21,3 +26,5 @@ Export-ModuleMember Remove-SDSCluster
 Export-ModuleMember New-SDSHosts
 Export-ModuleMember Get-SDSHostsInfo
 Export-ModuleMember Add-SDSClusterParameter
+Export-ModuleMember Get-SDSClusterNodes
+Export-ModuleMember Add-SDSClusterHost
